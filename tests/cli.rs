1c39ecc12404f2ee79ba576694c3c90e402a83d8
Junhson Jean-Baptiste <jjeanba2@binghamton.edu>
2025-04-23 08:34:17 -0400

    flesh out tests a little

--- tests/cli.rs
+++ tests/cli.rs
@@ -6,11 +6,27 @@ use predicates::prelude::*;
 
 #[test]
 fn show_help_no_args() -> Result<()> {
-    let mut cmd = Command::cargo_bin("ds").unwrap();
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::contains("Usage"));
-    Ok(())
+    run(&[], "Usage")
+}
+
+#[test]
+fn put_succeed_two_args() -> Result<()> {
+    run(&["put", "key", "value"], "")
+}
+
+#[test]
+fn get_success() -> Result<()> {
+    run(&["get", "key"], "")
+}
+
+#[test]
+fn list_success() -> Result<()> {
+    run(&["list"], "")
+}
+
+#[test]
+fn delete_success() -> Result<()> {
+    run(&["delete", "-n", "key"], "")
 }
 
 #[test]
@@ -33,13 +49,13 @@ fn put_fail_single_arg() -> Result<()> {
     Ok(())
 }
 
-#[test]
-fn put_succeed_two_args() -> Result<()> {
+// TODO: this currently uses an empty string to test only for success, I don't love that
+fn run(args: &[&str], substring: &str) -> Result<()> {
     Command::cargo_bin("ds")?
-        .args(["put", "key", "value"])
+        .args(args)
         .assert()
-        .success();
+        .success()
+        .stdout(predicate::str::contains(substring))
+        .stderr(predicate::str::is_empty());
     Ok(())
 }
-
-// TODO: write a run function to avoid redundancy
